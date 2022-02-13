779da9fe595eec5e31a32f57d319dc8bb9a01d10
Rocha <rochagondim@hotmail.com>
2022-02-13 17:42:53 -0300

    criando crud e rotas matriculas

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -8,5 +8,10 @@ router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 router.post('/pessoas', PessoaController.criaPessoa)
 router.put('/pessoas/:id', PessoaController.atualizaPessoa)
 router.delete('/pessoas/:id', PessoaController.apagaPessoa)
+router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula),
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula),
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
+
 
 module.exports = router
